25e0b2f4d1c8f4309b0a1c83b1a1153ff1b31ffa
Baidurjya-sikder <sikderbadurjya@gmail.com>
2025-10-08 12:46:11 +0530

    add register controller and bug fixes

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,5 +1,5 @@
 import {v2 as cloudinary} from "cloudinary"
-import fs from fs
+import fs from "fs"
 
 cloudinary.config({ 
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
@@ -7,12 +7,12 @@ cloudinary.config({
     api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-const uploadOnCloudinary = async (localfilePath) => {
+const uploadOnCloudinary = async (localFilePath) => {
     try {
-        if (!localfilePath) return null
+        if (!localFilePath) return null
         //upload the file on cloudinary
         const response = await cloudinary.uploader.upload
-        (localfilePath, {
+        (localFilePath, {
             resource_type: "auto"
         })
         // file has been uploaded successfull
@@ -21,7 +21,7 @@ const uploadOnCloudinary = async (localfilePath) => {
         return response;
 
     } catch (error) {
-        fs.unlinkSync(localfilePath) // remove the locally saved file as the upload operation got failed 
+        fs.unlinkSync(localFilePath) // remove the locally saved file as the upload operation got failed 
         return null;
     }
 }
